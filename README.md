092edbd8cf912234f195604d83b66cb5afc11048
xschen <xs0040@gmail.com>
2017-05-24 04:39:26 +0800

    Source code update

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # java-data-image
 Package provides java implementation of some common api of image manipulation in the form of data frame
+
+[![Build Status](https://travis-ci.org/chen0040/java-data-image.svg?branch=master)](https://travis-ci.org/chen0040/java-data-image) [![Coverage Status](https://coveralls.io/repos/github/chen0040/java-data-image/badge.svg?branch=master)](https://coveralls.io/github/chen0040/java-data-image?branch=master)
---
+++ src/main/java/com/github/chen0040/data/image/ImageDataFrameFactory.java
@@ -0,0 +1,85 @@
+package com.github.chen0040.data.image;
+
+
+import com.github.chen0040.data.frame.BasicDataFrame;
+import com.github.chen0040.data.frame.DataFrame;
+import com.github.chen0040.data.frame.DataRow;
+
+import java.awt.image.BufferedImage;
+import java.util.Random;
+
+
+/**
+ * Created by xschen on 10/7/2016.
+ */
+public class ImageDataFrameFactory {
+    public static int get_rgb(int alpha, int r, int g, int b)
+    {
+        return (alpha << 24) | (r << 16) | (g << 8) | b;
+    }
+
+    public static int get_r(int rgb)
+    {
+        return (rgb >> 16) & 0xff;
+    }
+
+    public static int get_b(int rgb)
+    {
+        return (rgb & 0xff);
+    }
+
+    public static int get_g(int rgb)
+    {
+        return (rgb >> 8) & 0xff;
+    }
+
+    public static int get_luminance(int r, int g, int b)
+    {
+        return (int)(r * 0.3+ g * 0.59 + b * 0.11);
+    }
+
+    public static int get_luminance(int rgb)
+    {
+        int r=((rgb >> 16) & 0xff);
+        int g=((rgb >> 8) & 0xff);
+        int b=(rgb & 0xff);
+
+        int gray=get_luminance(r, g, b);
+
+        return gray;
+    }
+
+    public static DataRow getPixelTuple(DataFrame context, int rgb){
+        DataRow row = context.newRow();
+
+        double r = get_r(rgb);
+        double g = get_g(rgb);
+        double b = get_b(rgb);
+        double l = get_luminance(rgb);
+        row.setCell("r", r);
+        row.setCell("g", g);
+        row.setCell("b", b);
+        row.setCell("l", l);
+
+        return row;
+    }
+
+    private static Random rand = new Random();
+
+    public static DataFrame dataFrame(BufferedImage img){
+        DataFrame batch = new BasicDataFrame();
+        for(int i=0; i < 3000; i++)
+        {
+            int x = rand.nextInt(img.getWidth());
+            int y = rand.nextInt(img.getHeight());
+
+            int rgb = img.getRGB(x, y);
+            DataRow row = getPixelTuple(batch, rgb);
+            batch.addRow(row);
+        }
+        batch.lock();
+        return batch;
+    }
+
+
+}
